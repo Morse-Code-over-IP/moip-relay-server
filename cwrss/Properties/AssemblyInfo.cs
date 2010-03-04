7e916097bbcea058d16c6825290f20e96b598b8e
dc3dreamer <dc3dreamer@95e6be90-4f67-4e0b-b742-57ee295af34b>
2010-03-04 02:29:44 +0000

    Committing from Mac OS X. Edited, built, and tested robot and ionosphere under Mono/MonoDevelop. Added missing module headers.

--- cwrss/Properties/AssemblyInfo.cs
+++ cwrss/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.6.4.0")]
-[assembly: AssemblyFileVersion("0.6.4.0")]
+[assembly: AssemblyVersion("0.6.5.0")]
+[assembly: AssemblyFileVersion("0.6.5.0")]
--- ionosphere/Properties/AssemblyInfo.cs
+++ ionosphere/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ionosphere")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ionosphere server for CWCom")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DC-3 Dreams, SP")]
-[assembly: AssemblyProduct("ionosphere")]
-[assembly: AssemblyCopyright("Copyright © DC-3 Dreams, SP 2010")]
+[assembly: AssemblyCompany("Robert B. Denny, Mesa, AZ <rdenny@dc3.com>")]
+[assembly: AssemblyProduct("Ionosphere")]
+[assembly: AssemblyCopyright("Creative Commons Attribution Non-Commercial Share Alike")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
